acc89b4965825cbd41b3a4c9a9c16080f4e7934e
Blust96 <q.bohain@gmail.com>
2020-06-16 17:39:41 +0200

    [INIT] Project init

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,34 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 
-Vue.use(VueRouter)
+import HomeView from '@/views/HomeView';
+import GameView from '@/views/GameView';
+import EndGameView from '@/views/EndGameView';
 
-  const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+Vue.use(VueRouter);
+
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: HomeView
+    },
+    {
+        path: '/game',
+        name: 'Game',
+        component: GameView
+    },
+    {
+        path: '/end',
+        name: 'End',
+        component: EndGameView
+    },
+];
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
-})
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
+});
 
-export default router
+export default router;
