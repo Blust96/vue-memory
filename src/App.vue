58f8fee0e29485f449280ea704d15fc63379f63f
Quentin Bohain <q.bohain@gmail.com>
2020-07-26 15:58:27 +0200

    [UPDATE] Fade transition between pages

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
     <div id="app">
-        <router-view />
+        <transition name="fade" mode="out-in">
+            <router-view />
+        </transition>
     </div>
 </template>
 
@@ -11,5 +13,13 @@
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
+}
+
+.fade-enter-active, .fade-leave-active {
+    transition: all .5s ease;
+}
+.fade-enter, .fade-leave-to {
+    opacity: 0;
+    transform: translateY(-2em);
 }
 </style>
